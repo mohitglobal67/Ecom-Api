d3a8227c4e93b80572757f6fcfefb925d9e5c270
Libu Mathew Thomas <libuthomas@Libus-MacBook-Pro.local>
2023-01-20 17:12:32 +0530

    img fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
-
+mongoose.set('strictQuery', true);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true },);
 
