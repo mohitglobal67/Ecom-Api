f47c149a76de07c89d0bdd97d01d96f4c0accfc3
Libu Mathew Thomas <libuthomas@Libus-MacBook-Pro.local>
2023-01-21 11:36:00 +0530

    21 jan

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -30,7 +30,7 @@ user_routes.use(express.static(__dirname));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join("/temp/public/userimages"));
+        cb(null, path.join("public/userimages"));
     },
 
     filename: function (req, file, cb) {
